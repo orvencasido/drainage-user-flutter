adcb013abbb3e5a3cd682990e214d4e6257613f6
Orven Casido <orvencasido@gmail.com>
2026-06-21 01:15:56 +0800

    corrected app icon

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Drainage Monitoring System',
+      title: 'Report Drainage',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
